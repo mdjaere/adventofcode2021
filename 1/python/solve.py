4e04d47b36e463d0b0eb722ecbb92e2ca11950d6
Martin D. Jaere <martin.jaere11@imperial.ac.uk>
2021-12-01 09:58:00 +0000

    Day 1

--- 1/python/solve.py
+++ 1/python/solve.py
@@ -1,5 +1,4 @@
 import fileinput
-
 nums = [int(line.strip()) for line in fileinput.input()]
 
 ups = 0
@@ -8,5 +7,17 @@ for num in nums[1:]:
     if num > last:
         ups += 1
     last = num
-print(ups)
+print(f"Part 1: {ups}")
 
+
+tri_ups = 0
+last = sum(nums[0:3])
+for i in range(len(nums)):
+    window = nums[1+i:4+i]
+    if len(window) < 3:
+        break
+    tri_sum = sum(window)
+    if tri_sum > last:
+        tri_ups += 1
+    last = tri_sum
+print(f"Part 2: {tri_ups}")
