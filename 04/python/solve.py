2a828006813660dccdad9e9b44d08f2007682358
Martin D. Jaere <martin.jaere11@imperial.ac.uk>
2021-12-04 22:59:45 +0000

    Day 4 Tidy

--- 04/python/solve.py
+++ 04/python/solve.py
@@ -1,48 +1,61 @@
 import sys
-from collections import OrderedDict
-import copy
+from dataclasses import dataclass
 
 infile = sys.argv[1] if len(sys.argv) > 1 else 'input'
 raw_input = open(infile).read()
 sections = raw_input.split("\n\n")
 
+
+@dataclass
+class Square:
+    value: int = 0
+    checked: bool = False
+
+
+class Board:
+    def __init__(self, section):
+        self.rows = [[Square(int(c.strip())) for c in row.split()]
+                     for row in section.split("\n")]
+        self.is_winner = False
+        self.winning_number = None
+
+    def add_number(self, number):
+        # Add number
+        match = False
+        for row_idx, row in enumerate(self.rows):
+            for col_idx, square in enumerate(row):
+                if square.value == number:
+                    square.checked = True
+                    match = True
+                if match:
+                    break
+            if match:
+                break
+        # Check board
+        if match:
+            # check row and column of match
+            if all(sq.checked for sq in self.rows[row_idx]) or all(sq.checked for sq in [row[col_idx] for row in self.rows]):
+                self.is_winner = True
+                self.winning_number = number
+        return self.is_winner
+
+    def result(self):
+        return sum(sq.value for row in self.rows for sq in row if not sq.checked) * self.winning_number
+
+
 numbers = [int(c) for c in sections[0].split(",")]
-boards = [[[{"value": int(c.strip()), "checked": False} for c in row.split()]
-           for row in board.split("\n")]for board in sections[1:]]
+boards = [Board(section) for section in sections[1:]]
 
-
-def is_winner(board):
-    for row in board:
-        if all(sq["checked"] for sq in row):
-            return True
-    for column in zip(*board):
-        if all(sq["checked"] for sq in column):
-            return True
-    return False
-
-
-winners = OrderedDict()
+winners = []
 
 for number in numbers:
-    for board_idx, board in enumerate(boards):
-        for row in board:
-            for square in row:
-                if square["value"] == number:
-                    square["checked"] = True
-        if is_winner(board):
-            if not board_idx in winners:
-                winners[board_idx] = {
-                    "board": copy.deepcopy(board), "number": number}
-
-winner_list = list(winners.values())
-first = winner_list[0]
-last = winner_list[-1]
+    for board in boards:
+        if board.is_winner:
+            continue
+        board.add_number(number)
+        if board.is_winner:
+            winners.append(board)
 
 
-def calc_result(item):
-    return sum(sq["value"]
-               for row in item["board"] for sq in row if not sq["checked"]) * item["number"]
-
-
-print("Part1:", calc_result(first))
-print("Part2:", calc_result(last))
+print("Part1:", winners[0].result())
+print("Part2:", winners[-1].result())
