0cdd4a5055cdda5e7283980ea2bde11386c0a567
Martin D. Jaere <martin.jaere11@imperial.ac.uk>
2021-12-07 10:45:26 +0000

    Day 07 revert

--- 07/python/solve.py
+++ 07/python/solve.py
@@ -4,7 +4,7 @@ import statistics as st
 infile = sys.argv[1] if len(sys.argv) > 1 else 'input'
 input = [int(x) for x in open(infile).read().split(",")]
 
-m = round(st.median(input))
+m = round(st.median_grouped(input))
 r1 = sum(abs(i-m) for i in input)
 print("Part1:", r1)
 
