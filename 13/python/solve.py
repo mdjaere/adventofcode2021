e8d01fa042c28cedb33e381cb6ccb74efe589124
Martin D. Jaere <martin.jaere11@imperial.ac.uk>
2021-12-13 10:26:12 +0000

    Day 13 namedtuple

--- 13/python/solve.py
+++ 13/python/solve.py
@@ -1,11 +1,15 @@
 import sys
 from itertools import chain
+from collections import namedtuple
 
 infile = sys.argv[1] if len(sys.argv) > 1 else 'input'
 
 raw = open(infile).read()
 raw_coordinates, raw_instructions = raw.split("\n\n")
-dots = {tuple([int(x) for x in line.split(',')])
+
+Dot = namedtuple("Dot", ["x", "y"])
+
+dots = {Dot(*[int(x) for x in line.split(',')])
         for line in raw_coordinates.split("\n")}
 instructions = [line.replace("fold along ", "").split(
     "=") for line in raw_instructions.split("\n")]
@@ -22,21 +26,20 @@ for index, cmd in enumerate(instructions):
                 range(cmd_value),
                 range(cmd_value, -1, -1)
             ))
-            if dot[1] == cmd_value:
+            if dot.y == cmd_value:
                 continue
             else:
-                d = dot[0], new_order[dot[1]]
-                new_dots.add(d)
+                new_dots.add(Dot(dot.x, new_order[dot.y]))
         if cmd_type == "x":
             new_order = tuple(chain(
                 range(cmd_value * 2, cmd_value, -1),
                 range(cmd_value, max(xs) + 1)
             ))
-            if dot[0] == cmd_value:
+            if dot.x == cmd_value:
                 continue
             else:
-                d = new_order[dot[0]] - (cmd_value + 1), dot[1]
-                new_dots.add(d)
+                new_dots.add(
+                    Dot(new_order[dot.x] - (cmd_value + 1), dot.y))
     dots = new_dots
     if index == 0:
         print("Part1:", len(dots))
@@ -48,7 +51,7 @@ xs, ys = zip(*dots)
 for x in range(min(xs), max(xs) + 1):
     row = []
     for y in range(min(ys), max(ys) + 1):
-        if tuple([x, y]) in dots:
+        if Dot(x, y) in dots:
             row.append("#")
         else:
             row.append(" ")
